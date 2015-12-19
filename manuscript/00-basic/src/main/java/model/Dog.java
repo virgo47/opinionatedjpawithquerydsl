d1195358b81c1983d7f12291ace41704431fc83c
Richard Virgo Richter <virgo47@gmail.com>
2015-12-19 23:12:23 +0100

    initial version of some JPA/Querydsl demo

---
+++ manuscript/00-basic/src/main/java/model/Dog.java
@@ -0,0 +1,14 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Dog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+}
