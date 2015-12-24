b3fdc0c1b21f5f0e27cee4a47b972d39a4bc8d88
Richard Virgo Richter <virgo47@gmail.com>
2015-12-24 15:05:33 +0100

    env.sh merged into start-h2.sh

--- manuscript/dbinit/env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export H2_JAR=$HOME/.m2/repository/com/h2database/h2/1.4.190/h2-1.4.190.jar
-export GROOVY_INIT="groovy -cp .:..:$H2_JAR"
-export JAVA="java"
--- manuscript/dbinit/start-h2.sh
+++ manuscript/dbinit/start-h2.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Set these variables as necessary
+H2_JAR=$HOME/.m2/repository/com/h2database/h2/1.4.190/h2-1.4.190.jar
+GROOVY_INIT="groovy -cp .:..:$H2_JAR"
+JAVA="java"
+
 if [ -z "$1" ]; then
 	echo "Provide name of the directory with create/init scripts as a parameter!"
 	exit
 fi
-
-source env.sh
 
 $JAVA -jar $H2_JAR &
 sleep 1
