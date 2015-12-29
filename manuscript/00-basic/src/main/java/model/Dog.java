31cf185c2ed5eb6e391a620c07d31fbabed09a6e
Richard Virgo Richter <virgo47@gmail.com>
2015-12-29 16:24:37 +0100

    code reformat to indent = 2 with spaces instead of tabs (don't know how to set tab size in leanpub generated outputs)

--- manuscript/00-basic/src/main/java/model/Dog.java
+++ manuscript/00-basic/src/main/java/model/Dog.java
@@ -8,33 +8,33 @@ import javax.persistence.Id;
 @Entity
 public class Dog {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
 
-	private String name;
+  private String name;
 
-	public Integer getId() {
-		return id;
-	}
+  public Integer getId() {
+    return id;
+  }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+  public void setId(Integer id) {
+    this.id = id;
+  }
 
-	public String getName() {
-		return name;
-	}
+  public String getName() {
+    return name;
+  }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  public void setName(String name) {
+    this.name = name;
+  }
 
-	@Override
-	public String toString() {
-		return "Dog{" +
-			"id=" + id +
-			", name='" + name + '\'' +
-			'}';
-	}
+  @Override
+  public String toString() {
+    return "Dog{" +
+      "id=" + id +
+      ", name='" + name + '\'' +
+      '}';
+  }
 }
