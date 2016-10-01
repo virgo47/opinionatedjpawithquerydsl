5bf2ac2a53a124557cb1c4cf90731eeb56184994
Richard Virgo Richter <virgo47@gmail.com>
2016-10-01 13:37:27 +0200

    restructuring the to-one-troubles chapter, dual mapping went up, separate section for read/update scenarios (not finished)

---
+++ manuscript/examples/many-to-one-eager/src/test/java/tests/ProjectionToDogAndMerge.java
@@ -0,0 +1,69 @@
+package tests;
+
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQuery;
+import modeltoone.Breed;
+import modeltoone.Dog;
+import modeltoone.DogBasicView;
+import modeltoone.QDog;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class ProjectionToDogAndMerge {
+
+  public static void main(String[] args) {
+    run("demo-el");
+    run("demo-hib");
+  }
+
+  private static void run(String persistenceUnitName) {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory(persistenceUnitName);
+    try {
+      EntityManager em = emf.createEntityManager();
+      prepareData(em);
+      emf.getCache().evictAll();
+
+      em.getTransaction().begin();
+      QDog d = QDog.dog;
+      Tuple result = new JPAQuery<Dog>(em)
+        .select(d.id, d.name)
+        .from(d)
+        .where(d.id.eq(1))
+        .fetchOne();
+
+      Dog dog = new Dog();
+      dog.setId(result.get(d.id));
+      dog.setName(result.get(d.name));
+      System.out.println("dog from tuple = " + dog);
+
+      // another select(s), including Breed table
+      dog = em.merge(dog);
+      dog.setName("Bassie");
+      System.out.println("dog after merge = " + dog);
+
+      // commit updates the whole entity setting the breed_id to null
+      em.getTransaction().commit();
+      em.close();
+    } finally {
+      emf.close();
+    }
+  }
+
+  private static void prepareData(EntityManager em) {
+    em.getTransaction().begin();
+
+    Breed collie = new Breed();
+    collie.setName("collie");
+    em.persist(collie);
+
+    Dog lassie = new Dog();
+    lassie.setName("Lassie");
+    lassie.setBreed(collie);
+    em.persist(lassie);
+
+    em.getTransaction().commit();
+    em.clear();
+  }
+}
